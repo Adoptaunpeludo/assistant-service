8b64b9c98a917c892a7e631b6a0d34ec9d037588
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-25 15:55:49 +0100

    feat: server class

---
+++ src/presentation/server.ts
@@ -0,0 +1,48 @@
+import express, { Router } from 'express';
+
+interface Options {
+  port: number;
+  routes: Router;
+}
+
+export class Server {
+  public readonly app = express();
+  public serverListener?: any;
+  private readonly port: number;
+  private readonly routes: Router;
+
+  constructor(options: Options) {
+    const { port, routes } = options;
+
+    this.port = port;
+    this.routes = routes;
+  }
+
+  async start() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
+    this.serverListener = this.app.listen(this.port, async () => {
+      try {
+        console.log(`Server running on port ${this.port}`);
+      } catch (error) {
+        console.log(error);
+        console.log('There was an error starting the server');
+      }
+    });
+  }
+
+  public stop() {
+    return new Promise((resolve, reject) => {
+      this.serverListener.close((error: Error | undefined) => {
+        if (error) {
+          console.log({ error });
+          reject('There was an error closing the server');
+        } else {
+          console.log('Server closed');
+          resolve(true);
+        }
+      });
+    });
+  }
+}
