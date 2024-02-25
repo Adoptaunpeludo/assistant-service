5201b51366913d1ab659007baa416f0a8a689fbc
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-25 16:38:47 +0100

    feat: chatbot service complete

--- src/presentation/chat/service.ts
+++ src/presentation/chat/service.ts
@@ -1,5 +1,8 @@
 import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
-import { RunnablePassthrough } from '@langchain/core/runnables';
+import {
+  RunnablePassthrough,
+  RunnableSequence,
+} from '@langchain/core/runnables';
 import { PromptTemplate } from '@langchain/core/prompts';
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { PrismaVectorStore } from '@langchain/community/vectorstores/prisma';
@@ -25,7 +28,7 @@ export class ChatService {
     });
   }
 
-  private generateStandAloneQuestion() {
+  private generateStandAloneQuestionChain() {
     const standAloneQuestionTemplate = `Given a question, convert it to a standalone question.
     question: {question}
     standalone question:`;
@@ -41,7 +44,7 @@ export class ChatService {
     return standAloneQuestionChain;
   }
 
-  private retriever() {
+  private generateRetrieverChain() {
     const vectorStore = new PrismaVectorStore(this.embeddings, {
       db: prisma,
       prisma: Prisma,
@@ -53,10 +56,54 @@ export class ChatService {
       },
     });
 
-    return vectorStore.asRetriever();
+    const retriever = vectorStore.asRetriever();
+
+    const retrieverChain = RunnableSequence.from([
+      (prevResult) => prevResult.standaloneQuestion,
+      retriever,
+      this.combineDocuments,
+    ]);
+
+    return retrieverChain;
   }
 
   private combineDocuments(docs: Document[]) {
     return docs.map((doc) => doc.pageContent).join('\n\n');
+  }
+
+  private generateAnswerChain() {
+    const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Adoptaunpeludo based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email adoptaunpeludo@gmail.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+      context: {context}
+      question: {question}
+      answer:`;
+
+    const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
+    const answerChain = answerPrompt.pipe(this.model).pipe(this.stringParser);
+
+    return answerChain;
+  }
+
+  public async getChatBotAnswer(question: string) {
+    const standAloneQuestionChain = this.generateStandAloneQuestionChain();
+    const retrieverChain = this.generateRetrieverChain();
+    const answerChain = this.generateAnswerChain();
+
+    const chain = RunnableSequence.from([
+      {
+        standaloneQuestion: standAloneQuestionChain,
+        originalQuestion: this.passThrough,
+      },
+      {
+        context: retrieverChain,
+        question: ({ originalQuestion }) => originalQuestion.question,
+      },
+      answerChain,
+    ]);
+
+    const response = await chain.invoke({
+      question,
+    });
+
+    return response;
   }
 }
