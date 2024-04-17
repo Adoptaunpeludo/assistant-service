5e778a9a77df41810c964538cc481e0baa7ea3fe
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-17 17:17:29 +0200

    feat: rate limiter

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,12 +3,21 @@ import { ChatbotService } from './chatbot/service';
 import { envs } from '../config/envs';
 import { ChatbotController } from './chatbot/controller';
 import { ChatbotRoutes } from './chatbot/routes';
+import rateLimiter from 'express-rate-limit';
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.use('/api/chat', ChatbotRoutes.routes);
+    const apiLimiter = rateLimiter({
+      windowMs: 60 * 1000,
+      max: 100,
+      message: {
+        msg: 'Máximo de peticiones alcanzado, reintentalo tras 1 minuto',
+      },
+    });
+
+    router.use('/api/chat', apiLimiter, ChatbotRoutes.routes);
 
     return router;
   }
